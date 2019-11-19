d2ff8db69c6bdb5c0ef86e4db8422a5bf0adc433
Aline L <aline.lanckneus@live.be>
2019-11-19 16:43:29 +0100

    playing around with css

--- script.js
+++ script.js
@@ -1,7 +1,9 @@
+const key = '2bb393aebb088dea340a936c4e15a222';
+
 let input = document.querySelector('.input_text');
 let button = document.querySelector('.submit');
-let container1 = document.querySelector(".container1");
-const key = '2bb393aebb088dea340a936c4e15a222';
+let box1 = document.querySelector(".box1");
+let box2 = document.querySelector(".box2");
 let today = new Date();
 today = today.toLocaleDateString('en-US',{weekday: 'long', year: 'numeric', month: 'long', day: 'numeric'});
 
@@ -16,7 +18,7 @@ async function getWeather() {
     const res = data.list;
             console.log(res);
             let headerCity = document.createElement("h1");
-            container1.appendChild(headerCity);
+            box1.appendChild(headerCity);
             headerCity.innerHTML = `The weather for: ${input.value}`;
             let dateHeader = document.createElement("h2");
             headerCity.append(dateHeader);
@@ -38,20 +40,30 @@ async function getWeather() {
                     console.log(typeof date);
                     
                     let newDiv = document.createElement("div");
-                    let dateP = document.createElement("span");
-                    let newContent = document.createTextNode(' Temperature: '+ temp+ ' '+desc);
+                    newDiv.className += "miniBox";
+                    let dateP = document.createElement("div");
+                    let tempContent = document.createElement("div");
+                    tempContent.innerHTML= 'Temperature: '+ temp;
+
+                    let descElement = document.createElement("div");
+                    descElement.innerHTML = desc;
+
                     let iconImage = document.createElement("img");
                     iconImage.src = icon;
-                    let windSpeed = document.createTextNode('Windspeed: ' +wind + ' m/s ');
+
+                    let windSpeedEl = document.createElement("div");
+                    windSpeedEl.innerHTML = 'Windspeed: ' +wind + ' m/s ';
                     
                     dateP.innerHTML= date.slice(0,10);
+
                     newDiv.append(dateP);
+                    dateP.append(iconImage);
+                    newDiv.appendChild(tempContent);
+                    newDiv.appendChild(descElement);
+                    //newDiv.append(iconImage);
+                    descElement.append(windSpeedEl);
                     
-                    newDiv.appendChild(newContent);
-                    newDiv.append(iconImage);
-                    newDiv.append(windSpeed);
-                    
-                    container1.appendChild(newDiv);
+                    box2.appendChild(newDiv);
                 })();//end of makeElement function
             }//end of for loop
         };//end of getWeather function
