e12d46540c586208d6a68340a7a8d7afc7b6b3e8
jasper-springael <56021344+jasper-springael@users.noreply.github.com>
2019-11-19 14:48:28 +0100

    not final, tryouts

--- script2.js
+++ script2.js
@@ -1,39 +1,58 @@
 let input = document.querySelector('.input_text');
-let main = document.querySelector('#name');
-let temp = document.querySelector('.temp');
-let desc = document.querySelector('.desc');
-let clouds = document.querySelector('.clouds');
 let button= document.querySelector('.submit');
 const key = '2bb393aebb088dea340a936c4e15a222';
-let urlForecast = `https://samples.openweathermap.org/data/2.5/forecast?q=`+input.value+`&appid=`+key;
-
+let day = getday
 
 button.addEventListener('click', function(name){
-fetch('https://api.openweathermap.org/data/2.5/forecast?q='+input.value+'&appid='+key)
+
+  fetch('https://api.openweathermap.org/data/2.5/forecast?q='+input.value+'&appid='+key+'&units=metric')
+
 .then(response => response.json())
+
 .then(data => {
-         for(let i=0;i<data.list.length;i++) {
+  
+  (function addTitle(){
+    document.querySelector("#header-city").innerHTML = `The weather for: ${input.value}`;
+  })();
+
+  for(let i=0;i<data.list.length;i+=8){
             console.log(data.list[i]);
 
-            var tempValue = data.list[i]['main']['temp'];
-            var nameValue = data.city['name'];
-          //  var descValue = data.list[i]['weather'][i]['description'];
-          
-            main.innerHTML = nameValue;
-           // desc.innerHTML = "Desc - "+descValue;
-            temp.innerHTML = "Temp - "+tempValue;
-            input.value ="";
-         }
-            
+            (function getTemp () {
+              day= day++
+              let newDiv = document.createElement("div");
+              let temp =`${data.list[i]['main']['temp']}°`;
+              let desc= `${data.list[i]['weather'][0]['description']}`;
+              let newContent = document.createTextNode(temp+' '+desc);
+              console.log(newContent);
+              newDiv.appendChild(newContent);
+              let currentDiv = document.querySelector(".container1");
+              currentDiv.appendChild(newDiv);
+            })();
+        }
   console.log(data);
+
 })
 
 .catch(err => console.error(err));
 })
 
-const hallo = (data)=> {
-    for (item of data){
-        console.log(item);
-    }
+function getTemp (temp) {
+  let newDiv = document.createElement("div");
+  let newContent = document.createTextNode(data.list[i]['main']['temp']);
+  newDiv.appendChild(newContent);
+  let currentDiv = document.querySelector(".container1");
+  currentDiv.appendChild(newDiv);
 }
+
+// async solution to fetch
+// button.addEventListener("click", () => {
+//     getWeather()
+
+// })
+// async function getWeather(){
+//     const response = await fetch('https://api.openweathermap.org/data/2.5/forecast?q='+input.value+'&appid='+key);
+//     const data = await response.json();
+// }
+
 
